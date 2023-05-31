3ab6c688c5ce13135b5fe95ec6b42a08291d138a
vsisl <vaclav.sisl@gmail.com>
2023-05-31 19:49:37 +0200

    example python module showcasing the if __name__ == '__main__' clause

---
+++ main.py
@@ -0,0 +1,36 @@
+"""This is an example Python module.
+
+Python module is a text file with the .py extension. It contains Python code.
+
+main.py is a common name for the main Python module of a repository (an entry point) - the module the users are supposed
+to execute.
+"""
+
+
+# define a function
+def print_hi(name):
+    """This function greets the provided name.
+
+    :param name: str; name to greet
+    """
+    print(f'Hi, {name}')
+
+
+def main():
+    print_hi('IL')
+
+
+# The content of the following if-clause is only executed if this module is the module directly executed by Python.
+#  E.g. if we only imported the print_hi() function from this module, the content of this if-clause would not be
+#  executed.
+#
+# The if-clause indicates that this Python module is intended to be used as a script - i.e. it is intended to be
+#  directly executed. Other Python modules might not be intended for execution and might serve just as a library
+#  of Python objects to be used by other modules.
+#
+# Neither defining the main() function nor using the if __name__ == '__main__' clause is compulsory, however, it
+#  is a good indication to the user of your repository that this module is the entrypoint of the repo.
+#
+# See more on this topic here: https://www.youtube.com/watch?v=g_wlZ9IhbTs
+if __name__ == '__main__':
+    main()
